9c9435ba5676936d8607644db0d58755e774e1c3
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-04-22 21:22:42 +0200

    Enhance TodoRepository by adding import for List to support query methods

--- todo-api/src/main/java/com/example/todoapi/repository/TodoRepository.java
+++ todo-api/src/main/java/com/example/todoapi/repository/TodoRepository.java
@@ -2,6 +2,7 @@ package src.main.java.com.example.todoapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import src.main.java.com.example.todoapi.domain.Todo;
+import java.util.List;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
     List<Todo> findByCompleted(boolean completed);
