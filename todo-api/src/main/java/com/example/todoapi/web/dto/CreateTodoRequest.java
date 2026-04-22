f98824740bfd356a0c761cce066729813798db5b
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-04-22 21:31:03 +0200

    Implement CreateTodoRequest.java

---
+++ todo-api/src/main/java/com/example/todoapi/web/dto/CreateTodoRequest.java
@@ -0,0 +1,10 @@
+package src.main.java.com.example.todoapi.web.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record CreateTodoRequest(
+    @NotBlank
+    @Size(max = 500)
+    String title
+) {}
